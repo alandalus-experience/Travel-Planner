994072a562d903feece938f667ab6a6b8c39a379
smallidi <smallidi@cisco.com>
2020-09-10 17:35:44 +0530

    dispatch password reset action

--- client/components/User/ForgotPassword.js
+++ client/components/User/ForgotPassword.js
@@ -1,139 +1,149 @@
-import React from "react";
+import React, { useState } from "react";
+import { sendResetPassword } from "../../redux/actions/userActions";
+import { useDispatch } from "react-redux";
 
-const ForgotPassword = () => (
-
-	<div
-		className='recover-password'
-		style={{
-			position: "absolute",
-			top: "20%",
-			left: "10%",
-			height: "50%",
-			width: "80%",
-			boxShadow: "10px 10px 5px 5px rgba(0, 0, 0, 0.6)",
-			border: "1px solid black",
-			borderRadius: '5px'
-		}}>
-		{/* Display a Card that includes recovery instructions */}
+const ForgotPassword = () => {
+	const [email, setEmail] = useState('');
+	const dispatch = useDispatch();
+	return (
 		<div
-			className='recover-card'
+			className='recover-password'
 			style={{
-				height: "100%",
-				width: "100%",
-				display: "flex",
-				flexDirection: "column"
+				position: "absolute",
+				top: "20%",
+				left: "10%",
+				height: "50%",
+				width: "80%",
+				boxShadow: "10px 10px 5px 5px rgba(0, 0, 0, 0.6)",
+				border: "1px solid black",
+				borderRadius: '5px'
 			}}>
+			{/* Display a Card that includes recovery instructions */}
 			<div
-				className='recover-block'
+				className='recover-card'
 				style={{
+					height: "100%",
+					width: "100%",
 					display: "flex",
-					height: "20%",
-					width: "80%",
-					justifyContent: "center",
-					alignItems: "center",
-					margin: "0 auto",
+					flexDirection: "column"
 				}}>
-				<h2
+				<div
+					className='recover-block'
 					style={{
-						width: "100%",
-						height: "50%",
-						fontWeight: "bold",
-						textAlign: "center",
-						fontSize: '24px',
-						letterSpacing: "2px",
+						display: "flex",
+						height: "20%",
+						width: "80%",
+						justifyContent: "center",
+						alignItems: "center",
+						margin: "0 auto",
 					}}>
-					Recover Password
-				</h2>
-			</div>
-			<div
-				className='recover-block'
-				style={{
-					display: "flex",
-					height: "20%",
-					width: "80%",
-					justifyContent: "center",
-					alignItems: "center",
-					margin: "0 auto",
-				}}>
-				<span
+					<h2
+						style={{
+							width: "100%",
+							height: "50%",
+							fontWeight: "bold",
+							textAlign: "center",
+							fontSize: '24px',
+							letterSpacing: "2px",
+						}}>
+						Recover Password
+					</h2>
+				</div>
+				<div
+					className='recover-block'
 					style={{
-						width: "100%",
-						height: "50%",
-						fontSize: "14px",
-						fontStyle: "italic",
-						textAlign: "center"
+						display: "flex",
+						height: "20%",
+						width: "80%",
+						justifyContent: "center",
+						alignItems: "center",
+						margin: "0 auto",
 					}}>
-					Don 't worry, happens to the best of us
-				</span>
-			</div>
-			<div
-				className='recover-block'
-				style={{
-					display: "flex",
-					height: "20%",
-					width: "80%",
-					justifyContent: "center",
-					alignItems: "flex-end",
-					margin: "0 auto",
-				}}>
-				<span
+					<span
+						style={{
+							width: "100%",
+							height: "50%",
+							fontSize: "14px",
+							fontStyle: "italic",
+							textAlign: "center"
+						}}>
+						Don 't worry, happens to the best of us
+					</span>
+				</div>
+				<div
+					className='recover-block'
 					style={{
-						width: "100%",
-						height: "50%",
-						fontSize: "18px",
-						fontWeight: "bold",
-						textAlign: "center",
-						letterSpacing: "2px",
+						display: "flex",
+						height: "20%",
+						width: "80%",
+						justifyContent: "center",
+						alignItems: "flex-end",
+						margin: "0 auto",
 					}}>
-					Your email
-				</span>
-			</div>
-			<div
-				className='recover-block'
-				style={{
-					display: "flex",
-					height: "20%",
-					width: "80%",
-					justifyContent: "center",
-					alignItems: "center",
-					margin: "0 auto",
-				}}>
-				<input
-					type='text'
-					name='email'
+					<span
+						style={{
+							width: "100%",
+							height: "50%",
+							fontSize: "18px",
+							fontWeight: "bold",
+							textAlign: "center",
+							letterSpacing: "2px",
+						}}>
+						Your email
+					</span>
+				</div>
+				<div
+					className='recover-block'
 					style={{
-						width: "100%",
-						height: "50%",
+						display: "flex",
+						height: "20%",
+						width: "80%",
+						justifyContent: "center",
+						alignItems: "center",
+						margin: "0 auto",
 					}}>
-					
-				</input>
-			</div>
-			<div
-				className='recover-block'
-				style={{
-					display: "flex",
-					height: "20%",
-					width: "80%",
-					justifyContent: "center",
-					alignItems: "center",
-					margin: "0 auto",
-				}}>
-				<button
-					type='submit'
+					<input
+						type='text'
+						name='email'
+						style={{
+							width: "100%",
+							height: "50%",
+						}}
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					>
+					</input>
+				</div>
+				<div
+					className='recover-block'
 					style={{
-						width: "100%",
-						height: "60%",
-						backgroundColor: "black",
-						color: "white",
-						fontSize: "12px",
-						letterSpacing: "2px",
-						borderRadius: '5x'
+						display: "flex",
+						height: "20%",
+						width: "80%",
+						justifyContent: "center",
+						alignItems: "center",
+						margin: "0 auto",
 					}}>
-					Email me a recovery link
-				</button>
+					<button
+						type='submit'
+						style={{
+							width: "100%",
+							height: "60%",
+							backgroundColor: "black",
+							color: "white",
+							fontSize: "12px",
+							letterSpacing: "2px",
+							borderRadius: '5x',
+							cursor: "pointer"
+						}}
+						onClick={() => dispatch(sendResetPassword(email))}
+					>
+						Email me a recovery link
+					</button>
+				</div>
 			</div>
 		</div>
-	</div>
-);
+	)
+};
 
 export default ForgotPassword;
