51a61d68f969d344dcc02b1f75105e94e9c49a0c
Gabor Csecsetka <csecsi85@gmail.com>
2020-09-20 16:03:34 +0100

    Implemented user check so only existing users can create trips

--- api/controllers/controller-trip.js
+++ api/controllers/controller-trip.js
@@ -19,21 +19,27 @@ exports.createTrip = async (req, res) => {
 		};
 
 		//TODO:
-		// Check if the user_id corresponds to an existing user
-		// Add two-way binding to between the trip and the user
 		// Add a way to let more users get assigned to the same trip
-		const trip = new Trip(query);
 
 		let user = await User.findById(query.user_id);
-		user.trip_id.push(trip.id);
 
-		await user.save();
-		await trip.save();
-		// await user.save();
+		if (user) {
+			const trip = new Trip(query);
+			user.trip_id.push(trip.id);
 
-		return res.status(201).json({
-			message: 'Trip created',
-		});
+			await user.save();
+			await trip.save();
+
+			return res.status(201).json({
+				message: 'Trip created',
+			});
+		} else {
+			return res.status(401).json({
+				status: 401,
+				message: 'User not found'
+			});
+		}
+
 	} catch (error) {
 		// TODO: Add proper error handling
 		res.status(500).json({
