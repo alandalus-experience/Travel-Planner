7e1961a5cde168e067085e3ca363274b514e0f12
Gabor Csecsetka <csecsi85@gmail.com>
2020-09-23 02:17:57 +0100

    Changed to fixer.io rates API because of better currency support

--- cron-jobs/fetchDailyExchangeRates.js
+++ cron-jobs/fetchDailyExchangeRates.js
@@ -5,7 +5,9 @@ const Rates = require('../api/models/model-rates');
 
 exports.fetchDailyExchangeRates = cron.schedule('0 1 * * *', async () => {
 	try {
-		const res = await fetch('https://api.exchangeratesapi.io/latest');
+		const res = await fetch(
+			`http://data.fixer.io/api/latest?access_key=${process.env.FIXER_ACCESS_KEY}`
+		);
 		const resJSON = await res.json();
 		const rates = new Rates(resJSON);
 		await rates.save();
