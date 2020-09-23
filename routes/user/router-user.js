f4ffe60eb29d9a4a6ad272fd70dbdd7a2f8e334d
Gabor Csecsetka <csecsi85@gmail.com>
2020-09-23 02:56:11 +0100

    Added delete user support and removed user from response

--- routes/user/router-user.js
+++ routes/user/router-user.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
-const { registerUser, loginUser } = require('../../api/controllers/controller-user');
+const { registerUser, loginUser, deleteUser } = require('../../api/controllers/controller-user');
 
 const router = express.Router();
 
 router.route('/register').post(registerUser);
 
 router.route('/login').post(loginUser);
+
+router.route('/delete').delete(deleteUser);
 
 module.exports = router;
