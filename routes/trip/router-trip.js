18ac422c35d81ef7a5468501c1b44b6ffdd19bb4
Gabor Csecsetka <csecsi85@gmail.com>
2020-09-24 10:35:30 +0100

    Feature: remove user of a trip

--- routes/trip/router-trip.js
+++ routes/trip/router-trip.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
-const { createTrip, addUserToTrip, deleteTrip } = require('../../api/controllers/controller-trip');
+const { createTrip, addUserToTrip, removeUserFromTrip, deleteTrip } = require('../../api/controllers/controller-trip');
 
 const router = express.Router();
 
 router.route('/create').post(createTrip);
 router.route('/delete').delete(deleteTrip);
 router.route('/adduser').patch(addUserToTrip);
+router.route('/removeuser').patch(removeUserFromTrip);
 
 module.exports = router;
