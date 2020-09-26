34d3ccc57b2f86f21dfce312ece4a7cfb7b20dfa
Gabor Csecsetka <csecsi85@gmail.com>
2020-09-26 06:55:13 +0100

    Added function to get all trips for a user so we can use real data

--- routes/trip/router-trip.js
+++ routes/trip/router-trip.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
-const { createTrip, addUserToTrip, removeUserFromTrip, deleteTrip } = require('../../api/controllers/controller-trip');
+const { createTrip, getTrips, addUserToTrip, removeUserFromTrip, deleteTrip } = require('../../api/controllers/controller-trip');
 
 const router = express.Router();
 
+router.route('/gettrips').post(getTrips);
 router.route('/create').post(createTrip);
 router.route('/delete').delete(deleteTrip);
 router.route('/adduser').patch(addUserToTrip);
